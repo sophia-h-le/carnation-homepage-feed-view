4e391532fe5f31ca6f110495808d52333c26443b
Sophia Le <sophiale@sophias-mbp.lan>
2021-06-19 18:05:48 -0700

    Add list of user communities on side menu

--- src/components/Body/BodyofFeed/SideMenu/Communities.js
+++ src/components/Body/BodyofFeed/SideMenu/Communities.js
@@ -1,12 +1,35 @@
 import React from 'react';
-// import './Communities.css';
+import './Communities.css';
 
 
 const Communities = () => {
+    const EXAMPLE_COMMUNITIES = [
+        {
+            label: 'Wellness',
+            className: 'community-link',
+            url: '#'
+        },
+        {
+            label: 'Sleep',
+            className: 'community-link',
+            url: '#'
+        },
+        {
+            label: 'Adoption',
+            className: 'community-link',
+            url: '#'
+        }
+    ]
     return (
-        <div>
-            <div></div>
-        </div>
+        <ul className='community-menu'>
+            {EXAMPLE_COMMUNITIES.map((communityItem, index) =>
+                <li key={index}>
+                    <a className={communityItem.className} href={communityItem.url}>
+                        {communityItem.label}
+                    </a>
+                </li>
+            )}
+        </ul>
     );
 }
 
