2388869ee154c10e5030b31d5ad7855935075402
Sophia Le <sophiale@sophias-mbp.lan>
2021-06-19 17:05:50 -0700

    Update homepage/feed view

---
+++ src/components/Body/BodyofFeed/SideMenu/Communities.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './Communities.css';
+
+
+const Communities = () => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Communities;
--- src/components/Body/BodyofFeed/SideMenu/SideMenu.js
+++ src/components/Body/BodyofFeed/SideMenu/SideMenu.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './SideMenu.css';
-import plusSign from './icons8-plus-96.png';
+// import plusSign from './icons8-plus-96.png';
+import Communities from './Communities';
 
 const SideMenu = () => {
     return (
         <div className='SideMenu'>
             <div className='title'>Your Communities</div>
-            <div>
-                {/* <img className='join-communities-icon' src={plusSign} alt="Join Communities icon" /> */}
-                <div className='join-communities-title'> + Join Communities</div>
-            </div>
+
+            <Communities/>
+
+            {/* <img className='join-communities-icon' src={plusSign} alt="Join Communities icon" /> */}
+            <div className='join-communities-title'> + Join Communities</div>
         </div>
     );
 }
